47bce4bfceb05ef5daabd85f4b891811f5182871
chloe <chloe.c.ning@gmail.com>
2026-02-22 20:52:33 -0500

    create linear regression

--- app_job_main.py
+++ app_job_main.py
@@ -1,10 +1,25 @@
 import logging
-
+import numpy as np
+from scipy import stats
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
+def regression():
+    a = 3.14
+
+    df = pd.read_csv("regression_input.csv")
+
+    x = df["x"].to_numpy()
+    y = df["y"].to_numpy()
+
+    result = stats.linregress(x, y)
+
+    return a * result.slope + result.intercept
+
 
 if __name__ == "__main__":
-    print("Hello Azure")
+    print(f"result: {regression()}")
+
     logger.info("Azure App Job has started.")
     logger.info("Azure App Job has completed.")
